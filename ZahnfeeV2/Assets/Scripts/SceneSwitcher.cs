d1b7d16e59cf575188f584629658f90249167302
elisabethragginger <elisabeth.ragginger@htl-salzburg.ac.at>
2019-03-19 17:02:05 +0100

    add Files

---
+++ ZahnfeeV2/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public void SceneSwitch()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Angeklickt");
+    }
+
+    public void SceneSwitch2()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("Angeklickt");
+    }
+}
