ec799f870d94e0cefe95bfcdf3730514d5e7caf8
thomasreindl <reindlthomas@gmx.at>
2019-03-20 12:26:21 +0100

    Cleaning update

--- ZahnfeeV2/Assets/Scripts/TestMono.cs
+++ ZahnfeeV2/Assets/Scripts/TestMono.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TestMono : MonoBehaviour
 {
-
     string myName = "elisabeth";
 
     // Use this for initialization
