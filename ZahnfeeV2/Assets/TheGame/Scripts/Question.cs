c6e9c1492230335d78f0a1c59f065acbea5d6ef2
smeerws <hello@smeerws.at>
2019-04-03 11:25:43 +0200

    update questions

--- ZahnfeeV2/Assets/TheGame/Scripts/Question.cs
+++ ZahnfeeV2/Assets/TheGame/Scripts/Question.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "QuestionData")]
-public class Question : ScriptableObject {
-
+public class Question : ScriptableObject
+{
     public string question = "";
+    //0 richtig, 1-3 falsch
     public string[] answers = new string[4];
-    
 }
