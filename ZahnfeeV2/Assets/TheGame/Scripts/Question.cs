1ca75ebcfa97d2f5919e05236bdddfb62d995bab
smeerws <hello@smeerws.at>
2019-04-03 11:12:18 +0200

    scriptable object added

---
+++ ZahnfeeV2/Assets/TheGame/Scripts/Question.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "QuestionData")]
+public class Question : ScriptableObject {
+
+    public string question = "";
+    public string[] answers = new string[4];
+    
+}
